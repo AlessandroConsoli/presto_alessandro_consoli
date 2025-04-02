6fdd27f916b547b3c423ce7acac8e0577e743611
Alessandro Consoli <alessandroconsoli.1982@gmail.com>
2025-04-02 16:37:16 +0200

    US 9 completata

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -3,10 +3,11 @@
 namespace App\Jobs;
 
 use Spatie\Image\Image;
+use Spatie\Image\Enums\Unit;
 use Spatie\Image\Enums\CropPosition;
 use Illuminate\Foundation\Queue\Queueable;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
 class ResizeImage implements ShouldQueue
 {
@@ -32,6 +33,14 @@ class ResizeImage implements ShouldQueue
 
         Image::load($srcPath)
         ->crop($w, $h, CropPosition::Center)
+        ->watermark(
+            base_path('resources/img/watermark.png'),
+            width: 100,
+            height: 100,
+            paddingX: 5,
+            paddingY: 5,
+            paddingUnit: Unit::Percent
+        )
         ->save($destPath);
 
     }
